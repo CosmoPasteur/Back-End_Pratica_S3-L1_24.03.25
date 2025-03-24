712f094c3fc83a13bb26cc71f44866770d4d503f
“Cosimo <pastore_cosimo@yahoo.it>
2025-03-24 17:06:08 +0100

    es 2

--- src/main/java/it/epicode/pgAdmin/Exercise.java
+++ src/main/java/it/epicode/pgAdmin/Exercise.java
@@ -4,7 +4,7 @@ public class Exercise {
     // Esercizio 1 / Estrai tutti i clienti con il nome Mario.
     // select * from clienti where nome = 'Mario';
     // Esercizio 2 / Estrarre nome e cognomedei clienti nati nel 1982.
-    // select nome, cognome, anno_di_nascita from clienti where anno_di_nascita = 1982;
+    // select nome, cognome from clienti where anno_di_nascita = 1982;
     // Esercizio 3 / Estrarre il numero delle fatture con iva al 20%.
     // select * from fatture where iva = 20;
     // Esercizio 4 / Estrarre prod.Attivi nel 2017 e che sono in produzione o commercio.
@@ -12,7 +12,7 @@ public class Exercise {
     // Esercizio 5 / Estrarre le fatture con importo inferiore a 1000 e i dati dei clienti ad esse collegate.
     // select numero_fattura, importo, id_cliente from fatture where importo < 1000;
     // Esercizo 6 / Rporta l'elenco delle fatture(numero, importo, iva e data) con il numero del fornitore.
-    // select numero_fattura, importo, iva, data_fattura from fatture join fornitori using (numero_fornitore)
+    // select numero_fattura, importo, iva, data_fattura from fatture join fornitori using (numero_fornitore);
     //
 
 }
